436086abb4381d2918c2b007b824e9de49ddf28e
derekc4859 <derekc4859@hstat.org>
2017-03-15 12:36:08 +0000

    Back End working

--- models/model.rb
+++ models/model.rb
@@ -5,58 +5,69 @@ require 'json'
 require 'pp'
 
 
-
-#url = 'http://pokeapi.co/api/v2/pokemon/?limit=150' #change ending to match pages with corresponding pokemon
-#uri = URI(url)
-#response = Net::HTTP.get(uri)
-#result = JSON.parse(response)
-#puts result["results"][0]["url"]
-#link = result["results"][0]["url"]
-#pp link
-#require 'open-uri'
-#content = open(link).read
-#content[user_name]
-#pp content[type]
-#require 'poke-api'
-
-@user_name = "bulbasaur"
-
-def find_pokemon_name(user_number)
-   if user_number > 0
-         user_number = user_number - 1
-   end
-    url = 'http://pokeapi.co/api/v2/pokemon/?limit=150' #change ending to match pages with corresponding pokemon
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    @result = JSON.parse(response)
-    @print_name = @result["results"][user_number]["name"]
-end
-def find_poke_info
-        link = @result["results"][0]["url"]
-        require 'open-uri'
-        content = open(link).read
-        pp content["forms"]
-end
-puts find_pokemon_name(1)
-puts find_poke_info
-
-
-
-#loader = Poke::API::Loader.new("pokemon")
-#loader.all
-# => [{ "name" => "Bulbasaur", "national_id" => 1, ... }, ...]
-#puts loader.find(169)
-# => { "name" => "Crobat", "national_id" => 169, ... }
-#Testing
-
-# Has to iterate through each different URL to find the id matching the users input
-#class ID
- #attr_reader :id
- 
-  # def initialize()
+#@user_name = "bulbasaur"
+class Name
     
- #  end
- 
+    attr_accessor :num , :form , :shiny_form
+    
+    def initialize(num)
+        @num = num.to_i
+    end
+    
+    def find_poke_name
+      number = @num
+      if number > 0
+        number -= 1
+      elsif number <= 0
+          number = number + 1
+      end
+      @real_num = number
+      url = 'http://pokeapi.co/api/v2/pokemon/?limit=150' #change ending to match pages with corresponding pokemon
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      @result = JSON.parse(response)
+      @print_name = @result["results"][@real_num]["name"]
+    end
+
+    def new_info
+     url = @result["results"][@real_num]["url"]
+    # url = "http://pokeapi.co/api/v2/pokemon/1/"
+     uri = URI(url) 
+     response = Net::HTTP.get(uri)
+     @link = JSON.parse(response)
+     @link["forms"][0]["url"]
+    end
+
+    def poke_sprites
+     url = @link["forms"][0]["url"]
+     uri = URI(url) 
+     response = Net::HTTP.get(uri)
+     @sprites = JSON.parse(response)
+     @form = @sprites["sprites"]["front_default"]
+     @shiny_form = @sprites["sprites"]["front_shiny"]
+    end
+
+    def poke_type
+     @typing = @link["types"][0]["type"]["name"]
+    end
+end
+
+
+#end
+#def poke_sprites
+ #  url = @link["forms"][0]["url"]
+ #  uri = URI(url) 
+#    response = Net::HTTP.get(uri)
+   # @sprites = JSON.parse(response)
+  # @form = @sprites["sprites"]["front_default"]
+ #  @shiny_form = @sprites["sprites"]["front_shiny"]
 #end
 
-
+# user_number = Name.new("150")
+# puts user_number.num
+# puts user_number.find_poke_name
+# puts user_number.new_info
+# user_number.poke_sprites
+# puts user_number.shiny_form
+# puts user_number.form
+# puts user_number.poke_type
